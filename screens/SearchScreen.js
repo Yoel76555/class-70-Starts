2722ea2f2f78f6ec39c8cf4b8e4c15ee8d351b16
Yoel76555 <71801083+Yoel76555@users.noreply.github.com>
2021-01-20 19:07:19 -0700

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class SearchScreen extends React.Component {
+
+  render() {
+    return (
+      <View style={{fex:1, justifyContent: 'center', alignItems:'center'}}>
+        <Text> Issue or Return </Text>
+      </View>
+    )
+  }
+}
